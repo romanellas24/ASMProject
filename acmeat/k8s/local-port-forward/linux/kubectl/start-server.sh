381f6eec826e6bce384d88d4ad39fa3d81ca2864
Giulio Zemignani <g.zemignani@studenti.unimol.it>
2025-05-13 13:15:50 +0200

    feat(k8s): added script for deployment and port forwarding

---
+++ acmeat/k8s/local-port-forward/linux/kubectl/start-server.sh
@@ -0,0 +1,12 @@
+#LANCH THIS FILE FROM A NEW TERMINAL AND NOT FROM VS CODE!!!!
+# https://stackoverflow.com/questions/75921414/java-symbol-lookup-error-snap-core20-current-lib-x86-64-linux-gnu-libpthread
+
+
+chmod +x *.sh
+
+gnome-terminal -- ./deliverycompanymanager-.sh 
+gnome-terminal -- ./menumanager-.sh
+gnome-terminal -- ./usermanager-.sh
+gnome-terminal -- ./ordermanager-.sh
+gnome-terminal -- ./localmanager-.sh
+
---
+++ acmeat/k8s/local-port-forward/linux/microk8s/start-server-microk8s.sh
@@ -0,0 +1,12 @@
+#LANCH THIS FILE FROM A NEW TERMINAL AND NOT FROM VS CODE!!!!
+# https://stackoverflow.com/questions/75921414/java-symbol-lookup-error-snap-core20-current-lib-x86-64-linux-gnu-libpthread
+
+
+chmod +x *.sh
+
+gnome-terminal -- ./deliverycompanymanager-microk8s.sh 
+gnome-terminal -- ./menumanager-microk8s.sh
+gnome-terminal -- ./usermanager-microk8s.sh
+gnome-terminal -- ./ordermanager-microk8s.sh
+gnome-terminal -- ./localmanager-microk8s.sh
+
