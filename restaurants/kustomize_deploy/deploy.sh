10b48aec276f1e7b9b8d39d4e38b5b66725ffc39
Tiestaa <francesco.testa3@studio.unibo.it>
2025-05-26 11:44:55 +0200

    deploy kustomize for 6 restaurants

--- restaurants/kustomize_deploy/deploy.sh
+++ restaurants/kustomize_deploy/deploy.sh
@@ -5,9 +5,9 @@ OVERLAY_DIR="./overlays"
 
 echo "deploy ingress controller nginx"
 
-# if not kubectl get deployment ingress-nginx-controller -n ingress-nginx >/dev/null 2>&1; then
-#    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/cloud/deploy.yaml
-# fi
+if not kubectl get deployment ingress-nginx-controller -n ingress-nginx >/dev/null 2>&1; then
+  microk8s kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/cloud/deploy.yaml
+fi
 
 echo "start deploy overlay"
 
@@ -17,10 +17,10 @@ for dir in "$OVERLAY_DIR"/*; do
     echo "Deploy for $NAME"
 
     echo "create namespace: $NAME"
-    kubectl apply -f "$dir/namespace.yaml"
+    microk8s kubectl apply -f "$dir/namespace.yaml"
 
     echo "user kustomize overlay of $NAME"
-    kubectl apply -k "$dir"
+    microk8s kubectl apply -k "$dir"
   fi
 done
 
