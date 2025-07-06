9f15120525428744075137f8ad24e38484f2f37c
Giulio Zemignani <g.zemignani@studenti.unimol.it>
2025-07-06 17:20:46 +0200

    to do implementation camunda

--- acmeat/acmeat-be/server/acmeat.domains.order/Services/OrderManagerService.cs
+++ acmeat/acmeat-be/server/acmeat.domains.order/Services/OrderManagerService.cs
@@ -18,6 +18,10 @@ using System.Globalization;
 using Telerik.JustMock;
 using Microsoft.Extensions.Options;
 using System.Threading;
+using Zeebe.Client;
+using NLog.Extensions.Logging;
+using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace acmeat.domains.order.Services;
 
@@ -37,7 +41,16 @@ public class GrpcOrderManagerService : server.order.manager.GrpcOrder.GrpcOrderB
     // private readonly DeliveryCompanyClient _deliveryCompanyClient;
     // private readonly UserClient _userClient;
     // private readonly DishClient _dishClient;
-    private readonly WaitingTimeLocalResponseOptions _waitingTimeLocalResponseOptions;
+    // private readonly WaitingTimeLocalResponseOptions _waitingTimeLocalResponseOptions;
+
+
+    private static readonly string ZeebeUrl = "127.0.0.1:26500";
+    private static readonly string ProcessInstanceVariables = "{\"a\":\"123\"}";
+    private static readonly string JobType = "payment-service";
+    private static readonly string WorkerName = Environment.MachineName;
+    private static readonly long WorkCount = 100L;
+
+    private ZeebeClient _zeebeClient;
     public GrpcOrderManagerService(
         ILogger<GrpcOrderManagerService> logger,
         OrderReader orderReader,
@@ -45,18 +58,24 @@ public class GrpcOrderManagerService : server.order.manager.GrpcOrder.GrpcOrderB
         // DeliveryCompanyClient deliveryCompanyClient,
         // UserClient userClient,
         // DishClient dishClient,
-        OrderDataWriter orderDataWriter,
-        IOptions<WaitingTimeLocalResponseOptions>  waitingTimeLocalResponseOptions
+        OrderDataWriter orderDataWriter
+        // IOptions<WaitingTimeLocalResponseOptions>  waitingTimeLocalResponseOptions
         )
     {
         _logger = logger;
         _orderDataWriter = orderDataWriter;
         _orderReader = orderReader;
+
+                _zeebeClient = (ZeebeClient)ZeebeClient.Builder()
+        .UseLoggerFactory(new NLogLoggerFactory())
+        .UseGatewayAddress(ZeebeUrl)
+        .UsePlainText()
+        .Build();
         // _deliveryCompanyClient = deliveryCompanyClient;
         // _userClient = userClient;
         // _dishClient = dishClient;
         // _localClient = localClient;
-        _waitingTimeLocalResponseOptions = waitingTimeLocalResponseOptions.Value;
+        // _waitingTimeLocalResponseOptions = waitingTimeLocalResponseOptions.Value;
     }
 
     public override Task<server.order.manager.HelloReplyClient> SayHello(server.order.manager.HelloRequestClient request, ServerCallContext context)
@@ -118,142 +137,171 @@ public class GrpcOrderManagerService : server.order.manager.GrpcOrder.GrpcOrderB
     {
         server.order.manager.GeneralResponse generalResponse = new server.order.manager.GeneralResponse();
 
-
-
-
-        var time = TimeSpan.Parse(order.DeliveryTime);
-        //CHECK IF THE ORDER IS BETWEEN 12-14 AND 19-22
-        if ((time.Hours < 12 || time.Hours > 14) && (time.Hours < 19 || time.Hours > 22))
+         var topology = await _zeebeClient.TopologyRequest()
+            .Send();
+        var worker =_zeebeClient
+        .NewWorker()
+        .JobType("CreateOrder")
+        .Handler(async (jobClient, job) =>
         {
-            generalResponse.Message = "Order must be between 12-14 and 19-22. Cannot proceed with order creation";
-            return generalResponse;
+
+            var time = TimeSpan.Parse(order.DeliveryTime);
+            //CHECK IF THE ORDER IS BETWEEN 12-14 AND 19-22
+            if ((time.Hours < 12 || time.Hours > 14) && (time.Hours < 19 || time.Hours > 22))
+            {
+                generalResponse.Message = "Order must be between 12-14 and 19-22. Cannot proceed with order creation";
+                throw new Exception(generalResponse.Message);
+            }
+
+            var deliveryTime = DateTime.Today.Add(time);
+            order.DeliveryTime = deliveryTime.ToString("dd-MM-yyyy HH:mm");
+
+
+
+
+
+
+            // server.order.Order serverOrder = ConvertGrpcToServerModel(order);
+            // db.order.Order dbOrder = serverOrder.Convert();
+            // Local local = await _localClient.GetLocalById(order.LocalId);
+            // User user = await _userClient.GetUserById(order.UserId);
+            // CancellationTokenSource s_cts = new CancellationTokenSource();
+
+            //LOCAL AVAILABILITY FLOW
+            // DishList dishList = await _dishClient.GetDishsByMenuId(dbOrder.MenuId);
+            // List<DishInfo> dishes = dishList.Dishs.AsEnumerable().Select(dish => new DishInfo(dish.Id, order.Quantity)).ToList();
+
+            // server.local.client.GeneralResponse localAvailabilityResponse = new server.local.client.GeneralResponse();
+            //OLD VERSION
+            // Task checkAvailabilityTask = new Task(async () =>
+            // {
+            //     localAvailabilityResponse = await _localClient.CheckOrderAvailability(dbOrder, dishes, local.Url);
+            // });
+
+
+            // var hasFinishedSuccesfully = checkAvailabilityTask.Wait(_waitingTimeLocalResponseOptions.Time * 1000);
+
+            // if something has went wrong local-side comunicate to the interface 
+            // if (hasFinishedSuccesfully && localAvailabilityResponse.Message != "OK")
+            // {
+
+            //     _logger.LogInformation("The local is unavailable the response took too long. Reason" + localAvailabilityResponse.Message);
+            //     generalResponse.Message = "The local is unavailable the response took too long. Reason" + localAvailabilityResponse.Message;
+            //     return generalResponse;
+            // }
+            // //if the request took to much time just cancel the flow
+            // else if (!hasFinishedSuccesfully)
+            // {
+            //     _logger.LogInformation("The local is unavailable the response took too long. ");
+            //     generalResponse.Message = "The local is unavailable the response took too long.";
+            //     return generalResponse;
+            // }
+
+
+            // try
+            // {
+            //     s_cts.CancelAfter(_waitingTimeLocalResponseOptions.Time * 1000);
+
+            //     localAvailabilityResponse = await _localClient.CheckOrderAvailability(dbOrder, dishes, local.Url, s_cts.Token);
+
+            //     if (localAvailabilityResponse.Message != "OK")
+            //     {
+
+            //         _logger.LogInformation("The local is unavailable the response took too long. Reason:" + localAvailabilityResponse.Message);
+            //         generalResponse.Message = "The local is unavailable the response took too long. Reason: " + localAvailabilityResponse.Message;
+            //         return generalResponse;
+            //     }
+            // }
+            // catch (OperationCanceledException)
+            // {
+            //     _logger.LogInformation("The local is unavailable the response took too long. ");
+            //     generalResponse.Message = "The local is unavailable the response took too long.";
+            //     return generalResponse;
+            // }
+            // finally
+            // {
+            //     s_cts.Dispose();
+            // }
+
+
+
+            //DELIVERY COMPANY FLOW
+
+            // DateTime startTime;
+            // // DateTime endTime;
+
+
+
+
+            // DeliveryCompanyList deliveryCompanyList = await _deliveryCompanyClient.GetDeliveryCompanyList();
+            // DeliveryCompanyList availableCompanies = new DeliveryCompanyList();
+
+
+            // foreach (DeliveryCompany deliveryCompany in deliveryCompanyList.Deliverycompanys)
+            // {
+            //     //TO DO:CHECK IF IT'S 10 KM RANGE
+            //     if (deliveryCompany.Address != null)
+            //     {
+            //         _logger.LogInformation($"Checking availability for deliveryCompany with id : {deliveryCompany.Id}");
+            //         startTime = DateTime.Now;
+            //         // TO DO COUNT UNTIL 15 THEN DELETE THE TASK IF IS NOT RESPONDING
+            //         // get veichle information or message justifying the unavailability
+            //         var deliveryCompanyResponse = await _deliveryCompanyClient.CheckAvailability(new AvailabilityPayload(local.Address, user.Address, deliveryTime.ToString(), deliveryCompany.Id));
+
+
+
+            //         if (deliveryCompanyResponse.Message == "OK")
+            //             availableCompanies.Deliverycompanys.Add(deliveryCompany);
+            //     }
+            // }
+
+            // if (availableCompanies.Deliverycompanys.Count <= 0)
+            //     throw new Exception("No delivery company is available");
+
+            // DeliveryCompany bestCompany = availableCompanies.Deliverycompanys.OrderBy(d => d.Price).First();
+
+            // _logger.LogInformation($"Delivery company selected  with id {bestCompany.Id}");
+
+            // await _deliveryCompanyClient.CommunicateOrder(new AvailabilityPayload(local.Address, user.Address, deliveryTime.ToString(), bestCompany.Id));
+
+
+            try
+            {
+                await _orderDataWriter.SendAsync(
+                 new CreateNewOrderCommand(
+                     ConvertGrpcToServerModel(order)
+                     )
+                 );
+                generalResponse.Message = "OK";
+
+            }
+            catch (Exception ex)
+            {
+                generalResponse.Message = ex.Message;
+                await jobClient.NewFailCommand(job.Key)
+                .Retries(0)
+                .ErrorMessage($"{ex.Message}")
+                .Send();
+            }
+            generalResponse = await Task.FromResult(
+                generalResponse
+            );
+
+
+
+            //SEND COMPLETE TASK TO ZEEGBE
+            // Dictionary<String, Object> outVars = new Dictionary<string, object>
+            // {
+            //     { "ProcessOrderResponse", order }
+            // };
+            var response = jobClient.NewCompleteJobCommand(job.Key)
+            .Send().GetAwaiter().GetResult()
+            ;
         }
+        )
+        .Open();
 
-        var deliveryTime = DateTime.Today.Add(time);
-        order.DeliveryTime = deliveryTime.ToString("dd-MM-yyyy HH:mm");
-        // server.order.Order serverOrder = ConvertGrpcToServerModel(order);
-        // db.order.Order dbOrder = serverOrder.Convert();
-        // Local local = await _localClient.GetLocalById(order.LocalId);
-        // User user = await _userClient.GetUserById(order.UserId);
-        // CancellationTokenSource s_cts = new CancellationTokenSource();
-
-        //LOCAL AVAILABILITY FLOW
-        // DishList dishList = await _dishClient.GetDishsByMenuId(dbOrder.MenuId);
-        // List<DishInfo> dishes = dishList.Dishs.AsEnumerable().Select(dish => new DishInfo(dish.Id, order.Quantity)).ToList();
-
-        // server.local.client.GeneralResponse localAvailabilityResponse = new server.local.client.GeneralResponse();
-        //OLD VERSION
-        // Task checkAvailabilityTask = new Task(async () =>
-        // {
-        //     localAvailabilityResponse = await _localClient.CheckOrderAvailability(dbOrder, dishes, local.Url);
-        // });
-
-
-        // var hasFinishedSuccesfully = checkAvailabilityTask.Wait(_waitingTimeLocalResponseOptions.Time * 1000);
-
-        // if something has went wrong local-side comunicate to the interface 
-        // if (hasFinishedSuccesfully && localAvailabilityResponse.Message != "OK")
-        // {
-
-        //     _logger.LogInformation("The local is unavailable the response took too long. Reason" + localAvailabilityResponse.Message);
-        //     generalResponse.Message = "The local is unavailable the response took too long. Reason" + localAvailabilityResponse.Message;
-        //     return generalResponse;
-        // }
-        // //if the request took to much time just cancel the flow
-        // else if (!hasFinishedSuccesfully)
-        // {
-        //     _logger.LogInformation("The local is unavailable the response took too long. ");
-        //     generalResponse.Message = "The local is unavailable the response took too long.";
-        //     return generalResponse;
-        // }
-
-
-        // try
-        // {
-        //     s_cts.CancelAfter(_waitingTimeLocalResponseOptions.Time * 1000);
-
-        //     localAvailabilityResponse = await _localClient.CheckOrderAvailability(dbOrder, dishes, local.Url, s_cts.Token);
-
-        //     if (localAvailabilityResponse.Message != "OK")
-        //     {
-
-        //         _logger.LogInformation("The local is unavailable the response took too long. Reason:" + localAvailabilityResponse.Message);
-        //         generalResponse.Message = "The local is unavailable the response took too long. Reason: " + localAvailabilityResponse.Message;
-        //         return generalResponse;
-        //     }
-        // }
-        // catch (OperationCanceledException)
-        // {
-        //     _logger.LogInformation("The local is unavailable the response took too long. ");
-        //     generalResponse.Message = "The local is unavailable the response took too long.";
-        //     return generalResponse;
-        // }
-        // finally
-        // {
-        //     s_cts.Dispose();
-        // }
-
-
-
-        //DELIVERY COMPANY FLOW
-
-        // DateTime startTime;
-        // // DateTime endTime;
-
-
-
-
-        // DeliveryCompanyList deliveryCompanyList = await _deliveryCompanyClient.GetDeliveryCompanyList();
-        // DeliveryCompanyList availableCompanies = new DeliveryCompanyList();
-
-
-        // foreach (DeliveryCompany deliveryCompany in deliveryCompanyList.Deliverycompanys)
-        // {
-        //     //TO DO:CHECK IF IT'S 10 KM RANGE
-        //     if (deliveryCompany.Address != null)
-        //     {
-        //         _logger.LogInformation($"Checking availability for deliveryCompany with id : {deliveryCompany.Id}");
-        //         startTime = DateTime.Now;
-        //         // TO DO COUNT UNTIL 15 THEN DELETE THE TASK IF IS NOT RESPONDING
-        //         // get veichle information or message justifying the unavailability
-        //         var deliveryCompanyResponse = await _deliveryCompanyClient.CheckAvailability(new AvailabilityPayload(local.Address, user.Address, deliveryTime.ToString(), deliveryCompany.Id));
-
-
-
-        //         if (deliveryCompanyResponse.Message == "OK")
-        //             availableCompanies.Deliverycompanys.Add(deliveryCompany);
-        //     }
-        // }
-
-        // if (availableCompanies.Deliverycompanys.Count <= 0)
-        //     throw new Exception("No delivery company is available");
-
-        // DeliveryCompany bestCompany = availableCompanies.Deliverycompanys.OrderBy(d => d.Price).First();
-
-        // _logger.LogInformation($"Delivery company selected  with id {bestCompany.Id}");
-
-        // await _deliveryCompanyClient.CommunicateOrder(new AvailabilityPayload(local.Address, user.Address, deliveryTime.ToString(), bestCompany.Id));
-
-
-        try
-        {
-            await _orderDataWriter.SendAsync(
-             new CreateNewOrderCommand(
-                 ConvertGrpcToServerModel(order)
-                 )
-             );
-            generalResponse.Message = "OK";
-
-        }
-        catch (Exception ex)
-        {
-            generalResponse.Message = ex.Message;
-        }
-        return await Task.FromResult(
-            generalResponse
-        );
-
-
+        return generalResponse;
 
 
 
@@ -387,3 +435,4 @@ public class GrpcOrderManagerService : server.order.manager.GrpcOrder.GrpcOrderB
 
     }
 }
+
