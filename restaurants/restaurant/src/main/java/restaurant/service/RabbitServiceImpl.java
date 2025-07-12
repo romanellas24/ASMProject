fba47b37feae787ed55c181a5d4f417dbd00cfee
Tiestaa <francesco.testa3@studio.unibo.it>
2025-07-12 17:39:01 +0200

    zeebe client and simplification of rabbit management

--- restaurants/restaurant/src/main/java/restaurant/service/RabbitServiceImpl.java
+++ restaurants/restaurant/src/main/java/restaurant/service/RabbitServiceImpl.java
@@ -19,9 +19,6 @@ import java.util.Map;
 public class RabbitServiceImpl implements RabbitService {
 
     @Autowired
-    private RabbitTemplate rabbitTemplate;
-
-    @Autowired
     private WSHandler wsHandler;
 
     @Autowired
@@ -30,48 +27,6 @@ public class RabbitServiceImpl implements RabbitService {
     @Autowired
     private ObjectMapper objectMapper;
 
-//    @Override
-//    public void publishDecision(DecisionOrderDTO decisionOrderDTO) {
-//        rabbitTemplate.convertAndSend(
-//                RabbitConfig.ORDER_EXCHANGE,
-//                RabbitConfig.DECISIONS_ROUTING_KEY,
-//                decisionOrderDTO
-//        );
-//    }
-//
-//    @Override
-//    @RabbitListener(queues = RabbitConfig.WAITING_ORDERS_QUEUE)
-//    public void handleWaitingOrder(OrderDTO waitingOrderDTO) {
-//        try {
-//            wsHandler.putOrder(waitingOrderDTO);
-//        } catch (Exception e) {
-////            this.publishDecision(new DecisionOrderDTO((String) waitingOrderDTO.getId(), false));
-//        }
-//    }
-//
-//    @Override
-//    @RabbitListener(queues = RabbitConfig.NEW_ORDERS_QUEUE)
-//    public void handleNewOrder(Integer id) throws Exception {
-//        OrderBasicInfoDTO orderBasicInfoDTO = new OrderBasicInfoDTO();
-//        orderBasicInfoDTO.setId(id);
-//        orderBasicInfoDTO.setDeleted(false);
-//        wsHandler.sendChangesInOrder(orderBasicInfoDTO);
-//    }
-//
-//    @Override
-//    @RabbitListener(queues = RabbitConfig.DELETED_ORDERS_QUEUE)
-//    public void handleDeletedOrder(Integer id) throws Exception {
-//        OrderBasicInfoDTO orderBasicInfoDTO = new OrderBasicInfoDTO();
-//        orderBasicInfoDTO.setId(id);
-//        orderBasicInfoDTO.setDeleted(true);
-//        wsHandler.sendChangesInOrder(orderBasicInfoDTO);
-//    }
-
-    //@Override
-    @RabbitListener(queues = RabbitConfig.TIMEOUT_ORDERS_QUEUE)
-    public void handleTimeoutOrder(Integer id) throws Exception {
-        wsHandler.deleteTimeoutOrder(id);
-    }
 
     @Override
     @RabbitListener(queues = RabbitConfig.EVENT_QUEUE)
--- restaurants/restaurant/src/main/java/restaurant/utils/RabbitConfig.java
+++ restaurants/restaurant/src/main/java/restaurant/utils/RabbitConfig.java
@@ -22,6 +22,10 @@ public class RabbitConfig {
     public final static String EVENT_ROUTING_KEY = "orders.key";
 
     @Bean
+    public TopicExchange orderExchange() {return new TopicExchange(ORDER_EXCHANGE);}
+
+
+    @Bean
     public Queue ordersQueue() {
         return new Queue(EVENT_QUEUE);
     }
@@ -42,80 +46,4 @@ public class RabbitConfig {
     public MessageConverter jackson2JsonMessageConverter(ObjectMapper objectMapper) {
         return new Jackson2JsonMessageConverter(objectMapper);
     }
-
-
-
-    // TODO: DELETE. USE ONLY EVENT QUEUE
-
-    public final static String WAITING_ORDERS_QUEUE="orders.waiting.queue";
-    public final static String DECISIONS_QUEUE="orders.decisions.queue";
-    public final static String DELETED_ORDERS_QUEUE="orders.deleted.queue";
-    public final static String NEW_ORDERS_QUEUE="orders.new.queue";
-    public final static String WAITING_ORDERS_ROUTING_KEY="order.new_waiting";
-    public final static String DECISIONS_ROUTING_KEY ="order.new_decision";
-    public final static String DELETED_ORDERS_ROUTING_KEY="order.deleted";
-    public final static String NEW_ORDERS_ROUTING_KEY="order.new";
-    public final static String TIMEOUT_ORDERS_QUEUE="orders.timeout";
-    public final static String TIMEOUT_ORDERS_ROUTING_KEY="orders.timeout.key";
-
-
-
-    @Bean
-    public TopicExchange orderExchange() {return new TopicExchange(ORDER_EXCHANGE);}
-
-    @Bean
-    public Queue waitingOrdersQueue() {return new Queue(WAITING_ORDERS_QUEUE);}
-
-    @Bean
-    public Queue decisionsQueue() {return new Queue(DECISIONS_QUEUE);}
-
-    @Bean
-    public Queue deletedOrdersQueue() {return new Queue(DELETED_ORDERS_QUEUE);}
-
-    @Bean
-    public Queue newOrdersQueue() {return new Queue(NEW_ORDERS_QUEUE);}
-
-    @Bean
-    public Queue timeoutQueue() {return new Queue(TIMEOUT_ORDERS_QUEUE);}
-
-    @Bean
-    public Binding waitingOrderBinding(Queue waitingOrdersQueue, TopicExchange orderExchange){
-        return BindingBuilder.bind(waitingOrdersQueue).to(orderExchange).with(WAITING_ORDERS_ROUTING_KEY);
-    }
-
-    @Bean
-    public Binding decisionBinding(Queue decisionsQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(decisionsQueue).to(exchange).with(DECISIONS_ROUTING_KEY);
-    }
-
-    @Bean
-    public Binding deletedOrderBinding(Queue deletedOrdersQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(deletedOrdersQueue).to(exchange).with(DELETED_ORDERS_ROUTING_KEY);
-    }
-
-    @Bean
-    public Binding newOrdersBinding(Queue newOrdersQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(newOrdersQueue).to(exchange).with(NEW_ORDERS_ROUTING_KEY);
-    }
-
-    @Bean
-    public Binding timeoutOrder(Queue timeoutQueue, TopicExchange exchange) {
-        return BindingBuilder.bind(timeoutQueue).to(exchange).with(TIMEOUT_ORDERS_ROUTING_KEY);
-    }
-
-//    @Bean
-//    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
-//        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
-//
-//        converter.setClassMapper(new DefaultJackson2JavaTypeMapper() {{
-//            setTrustedPackages("api.dto");
-//        }});
-//
-//        return converter;
-//    }
-//
-//    @Bean
-//    public MessageConverter messageConverter() {
-//        return jackson2JsonMessageConverter();
-//    }
 }
